9e8bed00b1fef0971ddf223130473c6b1f30e0f9
HopeMan000 <HopeMan9999@outlook.com>
2023-01-09 13:13:47 -0800

    second commit

---
+++ themewidget.h
@@ -0,0 +1,73 @@
+
+#ifndef THEMEWIDGET_H
+#define THEMEWIDGET_H
+
+#include <QtWidgets/QWidget>
+#include <QtCharts/QChartGlobal>
+#include "settingsdialog.h"
+#include "console.h"
+#include <QSerialPort>
+
+QT_BEGIN_NAMESPACE
+class QComboBox;
+class QCheckBox;
+class Ui_ThemeWidgetForm;
+QT_END_NAMESPACE
+
+QT_CHARTS_BEGIN_NAMESPACE
+class QChartView;
+class QChart;
+QT_CHARTS_END_NAMESPACE
+
+typedef QPair<QPointF, QString> Data;
+typedef QList<Data> DataList;
+typedef QList<DataList> DataTable;
+
+QT_CHARTS_USE_NAMESPACE
+
+class ThemeWidget: public QWidget
+{
+    Q_OBJECT
+public:
+    explicit ThemeWidget(QWidget *parent = 0);
+    ~ThemeWidget();
+
+private Q_SLOTS:
+    void updateUI();
+
+private:
+    DataTable generateRandomData(int listCount, int valueMax, int valueCount) const;
+    void populateThemeBox();
+    void populateAnimationBox();
+    void populateLegendBox();
+    void connectSignals();
+    QChart *createAreaChart() const;
+    QChart *createBarChart(int valueCount) const;
+    QChart *createPieChart() const;
+    QChart *createLineChart() const;
+    QChart *createSplineChart() const;
+    QChart *createScatterChart() const;
+
+private:
+    int m_listCount;
+    int m_valueMax;
+    int m_valueCount;
+    QList<QChartView *> m_charts;
+    DataTable m_dataTable;
+
+    Ui_ThemeWidgetForm *m_ui;
+
+private:
+	QChart *createTemperture1Chart() const;
+	QChart *createTemperture2Chart() const;
+	QChart *createPressureChart() const;
+
+	SettingsDialog *m_settings = nullptr;
+	QSerialPort *m_serial = nullptr;
+	Console *m_console = nullptr;
+
+public slots:
+	void btnSETTINGHandler();
+};
+
+#endif /* THEMEWIDGET_H */
